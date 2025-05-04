4d1e674181812b5851f61381297b8334ca57f3bf
rsalgadoc <rodrigo.salgado.cordova@gmail.com>
2025-05-04 00:06:37 -0400

    updata the next config format

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  images: { unoptimized: true }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
