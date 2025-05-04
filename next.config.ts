32de549fbff40bfe0053d88d4e9bf1f12631cded
rsalgadoc <rodrigo.salgado.cordova@gmail.com>
2025-05-04 00:38:56 -0400

    update the next conifng with the output dir

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",  // <=== enables static exports
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'out',
   reactStrictMode: true,
   images: { unoptimized: true }
-};
-
-module.exports = nextConfig;
+}
+ 
+export default nextConfig;
