f29e0f1b8115a0dc1ed1623c10a611bfe4043f2b
rsalgadoc <rodrigo.salgado.cordova@gmail.com>
2025-05-04 12:36:14 -0400

    remove unused sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { HeroSection } from './components/HeroSection';
-import { SkillsSection } from './components/SkillsSection';
 import { ProjectsSection } from './components/ProjectsSection';
-import { WebCapabilitiesSection } from './components/WebCapabilitiesSection';
 import { ContactSection } from './components/ContactSection';
 
 export default function WebDeveloperPortfolio() {
